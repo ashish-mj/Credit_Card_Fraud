f67d2bd6af191a4f6545c7411870768c0f1bb567
Ashish M J <ashm.jagadeesh@gmail.com>
2021-04-01 15:29:00 +0530

    Plot Amount Vs Class graph

--- detect.py
+++ detect.py
@@ -12,3 +12,10 @@ import seaborn as sb
 
 data = pd.read_csv("../creditcard.csv")
 print(data.head())
+
+fraud_transactions = data.loc[data["Class"]==1]
+print(len(fraud_transactions))
+non_fraud_transactions = data.loc[data["Class"]==0]
+print(len(non_fraud_transactions))
+
+sb.relplot(x="Amount",y="Class",data=data)
